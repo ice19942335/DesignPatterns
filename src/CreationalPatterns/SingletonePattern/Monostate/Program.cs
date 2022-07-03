398d21cf0b6b6ff2864fb2bfb3aeafebb0229b1f
Aleksejs Birula <aleksejs.birula@scandiweb.com>
2022-07-03 11:48:10 +0300

    Singleton - Monostate + Per-Thread Singleton

---
+++ src/CreationalPatterns/SingletonePattern/Monostate/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class CEO
+{
+    private static string _name;
+    private static int _age;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Age
+    {
+        get => _age;
+        set => _age = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var ceo = new CEO();
+        ceo.Name = "Adam Smith";
+        ceo.Age = 55;
+        
+        var ceo2 = new CEO();
+        
+        Console.WriteLine(ceo2);
+    }
+}
---
+++ src/CreationalPatterns/SingletonePattern/Per-Thread Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿public sealed class PerThreadSingleton
+{
+    private static ThreadLocal<PerThreadSingleton> _threadInstance =
+        new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+
+    public int Id;
+    
+    private PerThreadSingleton()
+    {
+        Id = Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public static PerThreadSingleton Instance => _threadInstance.Value;
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var t1 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("t1: " + PerThreadSingleton.Instance.Id);
+        });
+        
+        var t2 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+            Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+        });
+        Task.WaitAll(t1, t2);
+    }
+}
