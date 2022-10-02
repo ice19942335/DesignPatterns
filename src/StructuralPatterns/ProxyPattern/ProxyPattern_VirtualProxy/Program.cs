1f8290868412aba238f606b461d91dd2be236a31
Aleksejs Birula <aleksejs.birula@scandiweb.com>
2022-10-02 17:30:21 +0300

    ProxyPattern - Virtual proxy

---
+++ src/StructuralPatterns/ProxyPattern/ProxyPattern_VirtualProxy/Program.cs
@@ -0,0 +1,58 @@
+﻿public interface IBitmap
+{
+    void Draw();
+}
+
+public class Bitmap : IBitmap
+{
+    private readonly string _filename;
+
+    public Bitmap(string filename)
+    {
+        _filename = filename;
+        Console.WriteLine($"Loading image from {_filename}");
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine($"Drawing image {_filename}");
+    }
+}
+
+public class LazyBitmap : IBitmap
+{
+    private readonly string _filename;
+    private Bitmap _bitmap;
+
+    public LazyBitmap(string filename)
+    {
+        _filename = filename;
+    }
+    
+    public void Draw()
+    {
+        if (_bitmap == null)
+        {
+            _bitmap = new Bitmap(_filename);
+        }
+        
+        _bitmap.Draw();
+    }
+}
+
+public static class Program
+{
+    public static void DrawImage(IBitmap img)
+    {
+        Console.WriteLine("About to draw image");
+        img.Draw();
+        Console.WriteLine("Done drawing image");
+    }
+    
+    public static void Main(string[] args)
+    {
+        var img = new LazyBitmap("pokemon.png");
+        DrawImage(img);
+        
+    }
+}
---
+++ src/StructuralPatterns/ProxyPattern/ProxyPattern_VirtualProxy/README.md
@@ -0,0 +1,13 @@
+﻿##Actions order without proxy
+- Loading image from pokemon.png 
+- About to draw image
+- Drawing image pokemon.png
+- Done drawing image
+
+##Actions order with proxy
+- About to draw image
+- Loading image from pokemon.png
+- Drawing image pokemon.png
+- Done drawing image
+
+*As we can see now we loading image after the preparation message* 
